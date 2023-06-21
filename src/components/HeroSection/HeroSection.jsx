2d3b8252a75d30a45b3a4daea8935d563ba2aaf7
Swarada Bhosale <swaradapb113@gmail.com>
2023-06-21 16:05:50 +0530

    initial commit

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,213 @@
+import { motion } from 'framer-motion'
+import style from './HeroSection.module.css'
+
+const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i) => {
+    const delay = 1 + i * 0.2
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: 'spring', duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    }
+  },
+}
+const title = {
+  hidden: { y: -50, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+}
+
+const HeroSection = () => {
+  return (
+    <motion.div className={style.mainContainer}>
+      <motion.div className={style.titleContainer}>
+        <motion.h3
+          variants={title}
+          initial='hidden'
+          animate='visible'
+          transition={{ delay: 0.3 }}
+        >
+          Hello, I'm
+        </motion.h3>
+        <motion.h1
+          variants={title}
+          initial='hidden'
+          animate='visible'
+          transition={{ delay: 0.5 }}
+        >
+          Daulatrao Patil
+        </motion.h1>
+        <motion.h2
+          variants={title}
+          initial='hidden'
+          animate='visible'
+          transition={{ delay: 0.7 }}
+        >
+          aka PRO7ON
+        </motion.h2>
+      </motion.div>
+      <motion.div>
+        <motion.svg
+          height='85vh'
+          viewBox='0 0 600 600'
+          initial='hidden'
+          animate='visible'
+        >
+          <motion.circle
+            cx='100'
+            cy='100'
+            r='80'
+            stroke='#ff0055'
+            variants={draw}
+            custom={1}
+          />
+          <motion.line
+            x1='220'
+            y1='30'
+            x2='360'
+            y2='170'
+            stroke='#00cc88'
+            variants={draw}
+            custom={2}
+          />
+          <motion.line
+            x1='220'
+            y1='170'
+            x2='360'
+            y2='30'
+            stroke='#00cc88'
+            variants={draw}
+            custom={2.5}
+          />
+          <motion.rect
+            width='140'
+            height='140'
+            x='410'
+            y='30'
+            rx='20'
+            stroke='#0099ff'
+            variants={draw}
+            custom={3}
+          />
+          <motion.circle
+            cx='100'
+            cy='300'
+            r='80'
+            stroke='#0099ff'
+            variants={draw}
+            custom={2}
+          />
+          <motion.line
+            x1='220'
+            y1='230'
+            x2='360'
+            y2='370'
+            stroke='#ff0055'
+            custom={3}
+            variants={draw}
+          />
+          <motion.line
+            width='3rem'
+            x1='220'
+            y1='370'
+            x2='360'
+            y2='230'
+            stroke='#ff0055'
+            custom={3.5}
+            variants={draw}
+          />
+          <motion.rect
+            width='140'
+            height='140'
+            x='410'
+            y='230'
+            rx='20'
+            stroke='#00cc88'
+            custom={4}
+            variants={draw}
+          />
+          <motion.circle
+            cx='100'
+            cy='500'
+            r='80'
+            stroke='#00cc88'
+            variants={draw}
+            custom={3}
+          />
+          <motion.line
+            x1='220'
+            y1='430'
+            x2='360'
+            y2='570'
+            stroke='#0099ff'
+            variants={draw}
+            custom={4}
+          />
+          <motion.line
+            x1='220'
+            y1='570'
+            x2='360'
+            y2='430'
+            stroke='#0099ff'
+            variants={draw}
+            custom={4.5}
+          />
+          <motion.rect
+            width='140'
+            height='140'
+            x='410'
+            y='430'
+            rx='20'
+            stroke='#ff0055'
+            variants={draw}
+            custom={5}
+          />
+          {/* <motion.circle
+            cx='100'
+            cy='700'
+            r='80'
+            stroke='#ff0055'
+            variants={draw}
+            custom={6}
+          />
+          <motion.line
+            x1='220'
+            y1='630'
+            x2='360'
+            y2='770'
+            stroke='#00cc88'
+            variants={draw}
+            custom={7}
+          />
+          <motion.line
+            x1='220'
+            y1='770'
+            x2='360'
+            y2='630'
+            stroke='#00cc88'
+            variants={draw}
+            custom={7.5}
+          />
+          <motion.rect
+            width='140'
+            height='140'
+            x='410'
+            y='630'
+            rx='20'
+            stroke='#0099ff'
+            variants={draw}
+            custom={8}
+          /> */}
+        </motion.svg>
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default HeroSection
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from '../Sidebar/Sidebar'
+import HeroSection from '../HeroSection/HeroSection'
+import Skills from '../Skills/Skills'
+
+const Layout = () => {
+  return (
+    <div>
+      {/* <Sidebar /> */}
+      <Skills />
+      <HeroSection />
+    </div>
+  )
+}
+
+export default Layout
